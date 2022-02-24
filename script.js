bf55cac8942cbc484d48ea23a85454e33f14cc3d
Glenn Langdon <glennlangdon@gmail.com>
2022-02-24 15:10:37 -0600

    show ordered items

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ document.addEventListener('DOMContentLoaded', function () {
             this.name = name;
             this.price = price;
             this.num = num;
+            this.itemTotal = function() {
+                return (this.num * this.price);
+            }
         }
     }
     
+    // Class instances
     var hotdog = new OrderItem('hotdog', 4.5);
     var fries = new OrderItem('fries', 3.5);
     var soda = new OrderItem('soda', 1.5);
     var sauerkraut = new OrderItem('sauerkraut', 1);
 
     document.querySelector('input[type=submit]').addEventListener('click', function (e) {
+        // Clear div for repeat orders
+        document.querySelector('#ordered-items-list').innerHTML = "";
         // Associative array of quantities
         var quantities = getQuantity();
         // Add order quantities to array and calculate new array key values
@@ -26,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#order-total').innerHTML = "Your order total is: $" + total.toFixed(2);
         // Clear values from form
         document.querySelector('form').reset();
+        // Display ordered item
+        showOrderedItems(quantities);
         // Prevent page refresh
         e.preventDefault();
     });
@@ -46,7 +54,8 @@ document.addEventListener('DOMContentLoaded', function () {
         let prices = [];
         let allOrderItems = [hotdog, fries, soda, sauerkraut];
         for( let i = 0; i < allOrderItems.length; i++) {
-            prices.push(allOrderItems[i].price * q[i]);
+            allOrderItems[i].num = q[i];
+            prices.push(allOrderItems[i].itemTotal());
         }
         console.log(prices);
         return prices;
@@ -59,6 +68,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         console.log(total);
         return total;
+    }
+
+    function showOrderedItems(q) {
+        let allOrderItems = [hotdog, fries, soda, sauerkraut];
+        let orderList = document.querySelector('#ordered-items-list');
+        for (let i = 0; i < allOrderItems.length; i++) {
+            allOrderItems[i].num = q[i];
+            let li = document.createElement('li');
+            li.innerText = allOrderItems[i].num + ": " + allOrderItems[i].name;
+            orderList.appendChild(li);
+        }
     }
     console.log("DOM content parsed and loaded");
 }); // end DOMContentLoaded
